b40fcdb2c5430def3f12c1415b7f1c626012dbee
DimaMamaev <dmamaev@my.com>
2020-05-14 19:53:34 +0300

    creating helper functions to work with users array

--- server/index.js
+++ server/index.js
@@ -4,16 +4,30 @@ const http = require("http");
 
 const PORT = process.env.PORT || 5000;
 const router = require("./router");
-
+const { addUser, removeUser, getUser, getUsersInRoom } = require("./users");
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
 //Real-time connection/disconnection
 io.on("connection", (socket) => {
-  console.log("new connection detected");
   socket.on("join", ({ name, room }, callback) => {
-    console.log(name, room);
+    const { error, user } = addUser({ id: socket.id, name, room });
+    if (error) return callback(error);
+    socket.emit("message", {
+      user: "admin",
+      text: `${user.name} welcome to ${user.room}`,
+    });
+    socket.broadcast
+      .to(user.room)
+      .emit("message", { user: "admin", text: `${user.name} joined!` });
+    socket.join(user.room);
+    callback();
+  });
+
+  socket.on("sendMessage", (message, callback) => {
+    const user = getUser(socket.id);
+    io.to(user.room).emit("message", { user: user.name, text: message });
     callback();
   });
 
