61b3260ce6ec5504f6a14b68959bef9de4660119
DimaMamaev <dmamaev@my.com>
2020-05-15 22:19:29 +0300

    receiving first messages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,15 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { Chat } from "./components/Chat";
 import { Join } from "./components/Join";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Route path="/" exact component={Join} />
-      <Route path="/chat" component={Chat} />
+      <Switch>
+        <Route path="/" exact component={Join} />
+        <Route path="/chat" component={Chat} />
+      </Switch>
     </BrowserRouter>
   );
 };
--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -5,6 +5,8 @@ let socket;
 export const Chat = ({ location }) => {
   const [name, setName] = useState("");
   const [room, setRoom] = useState("");
+  const [messages, setMessages] = useState([]);
+  const [message, setMessage] = useState("");
   const ENDPOINT = "localhost:5000";
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
@@ -17,9 +19,37 @@ export const Chat = ({ location }) => {
       socket.off();
     };
   }, [ENDPOINT, location.search]);
+  useEffect(() => {
+    socket.on("message", (message) => {
+      setMessages([...messages, message]);
+    });
+  }, [messages]);
+
+  const sendMessage = (event) => {
+    event.preventDefault();
+    if (message) {
+      socket.emit("sendMessage", message, () => setMessage(""));
+    }
+  };
+  console.log(message, messages);
+
   return (
-    <div>
-      <h1>Chat</h1>
+    <div className="jumbotron jumbotron-fluid">
+      <div className="container">
+        <h1 className="display-4">Chat !</h1>
+        <div>
+          <input
+            type="text"
+            value={message}
+            onChange={(event) => setMessage(event.target.value)}
+            onKeyPress={(event) =>
+              event.key === "Enter" ? sendMessage(event) : null
+            }
+            className="form-control"
+            placeholder="type & send"
+          />
+        </div>
+      </div>
     </div>
   );
 };
