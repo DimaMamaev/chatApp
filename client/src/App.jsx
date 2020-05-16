75105948795942675f426a6cfdea35826c4cec75
DimaMamaev <dmamaev@my.com>
2020-05-16 19:48:16 +0300

    styling join component /  add validation to form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { Chat } from "./components/Chat/Chat";
 import { Join } from "./components/Join/Join";
+import ParticlesBg from "particles-bg";
+
 import "./App.css";
 
 const App = () => {
@@ -11,6 +13,7 @@ const App = () => {
         <Route path="/" exact component={Join} />
         <Route path="/chat" component={Chat} />
       </Switch>
+      <ParticlesBg type="circle" bg={true} />
     </BrowserRouter>
   );
 };
--- client/src/components/Join/Join.jsx
+++ client/src/components/Join/Join.jsx
@@ -1,33 +1,62 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
+import "./Join.css";
+
 export const Join = () => {
   const [name, setName] = useState("");
   const [room, setRoom] = useState("");
+
+  const onSubmitHandler = (event) => {
+    if (!name.trim() || !room.trim()) {
+      event.preventDefault();
+      return alert("Chat form is invalid! Try again!");
+    }
+  };
+
   return (
-    <div className="jumbotron jumbotron-fluid">
-      <div className="container">
-        <h1 className="display-4">Join</h1>
-        <input
-          type="text"
-          className="form-control"
-          placeholder="Name"
-          onChange={(event) => setName(event.target.value)}
-        />
-        <input
-          type="text"
-          className="form-control"
-          placeholder="Room"
-          onChange={(event) => setRoom(event.target.value)}
-        />
-        <Link
-          onClick={(event) => (!name || !room ? event.preventDefault() : null)}
-          to={`/chat?name=${name}&room=${room}`}
-        >
-          <button className="btn btn-primary" type="submit">
-            Sign In
-          </button>
-        </Link>
+    <div className="jumbotron jumbotron-fluid join">
+      <div className="container join__container">
+        <h1 className="join__article">Chat application!</h1>
+        <section>
+          <form
+            onSubmit={(event) => event.preventDefault()}
+            className="form-group join__form"
+          >
+            <label htmlFor="User_name" className="join__labels">
+              Name:
+            </label>
+
+            <input
+              type="text"
+              className="form-control join__input"
+              placeholder="Type your name"
+              id="User_name"
+              onChange={(event) => setName(event.target.value)}
+            />
+            <label htmlFor="User__room" className="join__labels">
+              Chat room:
+            </label>
+
+            <input
+              type="text"
+              className="form-control join__input"
+              placeholder="Type name of the chat room"
+              id="User__room"
+              onChange={(event) => setRoom(event.target.value)}
+            />
+
+            <Link
+              className="join__link"
+              onClick={(event) => onSubmitHandler(event)}
+              to={`/chat?name=${name}&room=${room}`}
+            >
+              <button className="btn btn-success join__btn" type="submit">
+                Sign In
+              </button>
+            </Link>
+          </form>
+        </section>
       </div>
     </div>
   );
